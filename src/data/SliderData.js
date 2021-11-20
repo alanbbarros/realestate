b2af07bb99b329f8e1462de566eef7d088361f4e
alanbbarros <alanbritobarros@gmail.com>
2021-11-20 03:21:12 -0300

    finished navbar, added all the images and other data and styled buttons

---
+++ src/data/SliderData.js
@@ -0,0 +1,39 @@
+import ImageOne from '../images/casa01.jpg';
+import ImageTwo from '../images/casa02.jpg';
+import ImageThree from '../images/casa03.jpg';
+import ImageFour from '../images/casa04.jpg';
+
+export const SliderData = [
+    {
+        title: 'Casa de Luxo 01, Rio de Janeiro',
+        price: 'R$8.500.000',
+        path: '/homes',
+        label: 'Veja a casa',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'Casa de Luxo 02, São Paulo',
+        price: 'R$8.500.000',
+        path: '/homes',
+        label: 'Veja a casa',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'Casa de Luxo 03, Goiânia',
+        price: 'R$8.500.000',
+        path: '/homes',
+        label: 'Veja a casa',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'Casa de Luxo 04, Brasília',
+        price: 'R$8.500.000',
+        path: '/homes',
+        label: 'Veja a casa',
+        image: ImageFour,
+        alt: 'House'
+    },
+]
